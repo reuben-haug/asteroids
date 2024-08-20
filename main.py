d7ed29e231f459f39636d54d030bea4f77d88240
reuben-haug <reuben.haug@gmail.com>
2024-08-19 20:11:59 -0700

    Added boilerplate main.py, constants.py, and requirements.txt

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        if event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
