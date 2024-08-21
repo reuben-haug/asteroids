8518e17cc7deacae23098c5ae4b3b4cab97b4c5e
reuben-haug <reuben.haug@gmail.com>
2024-08-20 20:46:15 -0700

    Added player spaceship using new CircleShape class

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 from constants import *
+from player import Player
 
 clock = pygame.time.Clock()
 dt = 0
 
-
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 def main():
     pygame.init()
@@ -12,16 +13,19 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    # 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
+        # Set frame rate to 60
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
