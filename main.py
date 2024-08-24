e7566f7caac4ac72ca7d918f3ff743a902f0f1b4
reuben-haug <reuben.haug@gmail.com>
2024-08-23 17:32:47 -0700

    Added Asteroid and AsteroidField classes.  Removed redundant attributes in Player class.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 clock = pygame.time.Clock()
 dt = 0
@@ -8,10 +10,16 @@ dt = 0
 # Set game groups
 updateable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroid = pygame.sprite.Group()
 
+# Set containers for each class
 Player.containers = (updateable, drawable)
+Asteroid.containers = (updateable, drawable, asteroid)
+AsteroidField.containers = (updateable)
 
+# Create game objects
 player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+asteroid_field = AsteroidField()
 
 def main():
     pygame.init()
@@ -33,13 +41,14 @@ def main():
         # Create black screen 
         screen.fill((0, 0, 0))
         
+        # Update and draw game objects
         for sprite in updateable:
             sprite.update(dt)
         for sprite in drawable:
             sprite.draw(screen)
 
+        # Update display
         pygame.display.flip()
-
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -6,7 +6,6 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         
-        self.position = pygame.Vector2(x, y)
         self.rotation = 0
 
         for group in self.containers:
